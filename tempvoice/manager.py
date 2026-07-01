d5c11a24e435177d38d091e1bedac5eb77c5ade1
kajlajk <70655887+kajlajk@users.noreply.github.com>
2026-07-01 03:25:51 +0200

    Update manager.py

--- tempvoice/manager.py
+++ tempvoice/manager.py
@@ -1,1 +1,16 @@
+import discord
+from discord.ext import commands
 
+from .config import *
+
+temp_channels = {}
+
+
+class TempVoiceManager(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+
+async def setup(bot):
+    await bot.add_cog(TempVoiceManager(bot))
