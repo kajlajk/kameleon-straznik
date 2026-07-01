45a9926641d201e9a129f571d88b7e98dd42a505
kajlajk <70655887+kajlajk@users.noreply.github.com>
2026-07-01 16:12:04 +0200

    Update modals.py

--- tempvoice/modals.py
+++ tempvoice/modals.py
@@ -1,1 +1,21 @@
+import discord
 
+
+class RenameModal(discord.ui.Modal, title="Zmień nazwę kanału"):
+
+    new_name = discord.ui.TextInput(
+        label="Nowa nazwa",
+        placeholder="Wpisz nazwę kanału...",
+        max_length=100
+    )
+
+    async def on_submit(self, interaction: discord.Interaction):
+
+        await interaction.user.voice.channel.edit(
+            name=self.new_name.value
+        )
+
+        await interaction.response.send_message(
+            "✅ Nazwa kanału została zmieniona.",
+            ephemeral=True
+        )
