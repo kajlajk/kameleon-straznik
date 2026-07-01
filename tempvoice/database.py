9f37b51f9bac9fa2cd86f215f9dbd7d68dda4d50
kajlajk <70655887+kajlajk@users.noreply.github.com>
2026-07-01 03:28:04 +0200

    Update database.py

--- tempvoice/database.py
+++ tempvoice/database.py
@@ -1,1 +1,17 @@
+import json
+import os
 
+FILE_NAME = "tempvoice_data.json"
+
+
+def load_data():
+    if not os.path.exists(FILE_NAME):
+        return {}
+
+    with open(FILE_NAME, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+def save_data(data):
+    with open(FILE_NAME, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
